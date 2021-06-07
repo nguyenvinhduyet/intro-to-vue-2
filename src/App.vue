028a4cdf371b54dd37cbf237f2c799179fe067df
VinhDuyet <nguyenvinhduyet@gmail.com>
2021-06-07 13:10:29 +0700

    Components (video 2/9) add App bar, Footer in our Application

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <v-app>
+
+<!-- App bar component -->
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text rounded>Home</v-btn>
+    <v-btn text rounded>Login</v-btn>
+  </v-app-bar>
+
+
+   <v-content>  <!-- v-content dùng để chỉ định phần tử bên trong có relative với phần tử gốc (app bar) tránh bị app bar đè lên -->
+<!-- login form -->
    <v-card width="400" class="mx-auto mt-5">
      <v-card-title>
        <h1 class="display-1">Login</h1>
      </v-card-title>
-     
      <v-card-text>
        <v-form>
          <v-text-field 
@@ -28,8 +39,38 @@
       <v-spacer></v-spacer>
       <v-btn color="info">Login</v-btn>
     </v-card-actions>
-
    </v-card>
+  </v-content>
+
+
+<!-- Footer component -->
+<v-footer
+    color="primary lighten-1"
+    padless
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-btn
+        v-for="link in links"
+        :key="link"
+        color="white" 
+        text  
+        rounded
+        class="my-2"
+      >
+        {{ link }}
+      </v-btn>
+      <v-col
+        class="primary lighten-2 py-4 text-center white--text"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Vuetify Dashboard</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+
   </v-app>
 </template>
 
@@ -39,12 +80,12 @@
 export default {
   name: 'App',
 
-  components: {
-
-  },
-
   data: () => ({
-    showPassword: false
+    showPassword: false,
+    links: [
+      'Home',
+      'Login'
+    ]
   }),
 };
 </script>
