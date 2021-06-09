7453ce035f4ae185b90d777d92ab5ff5556da674
VinhDuyet <nguyenvinhduyet@gmail.com>
2021-06-09 10:44:05 +0700

    Form Fundamentals - Signup Page (video 6)

--- src/router.js
+++ src/router.js
@@ -16,9 +16,6 @@ export default new Router({
 		{
 			path: '/about',
 			name: 'about',
-			// route level code-splitting
-			// this generates a separate chunk (about.[hash].js) for this route
-			// which is lazy-loaded when the route is visited.
 			component: () =>
 				import(/* webpackChunkName: "about" */ './views/About.vue')
 		},
@@ -31,6 +28,11 @@ export default new Router({
 			path: '/dashboard',
 			name: 'dashboard',
 			component: () => import('./views/Dashboard.vue')
-		}
+		},
+		{
+			path: '/signup',
+			name: 'signup',
+			component: () => import('./views/SignUp.vue')
+		},
 	]
 })
